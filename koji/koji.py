1fdcd67cf8ef5c120daa821d51034486546cd990
Arthur Assis <arthur120496@gmail.com>
2019-06-29 16:28:14 -0400

    Adds more cli params

--- koji/koji.py
+++ koji/koji.py
@@ -22,27 +22,27 @@ class Koji():
 
         system(cmd)
 
-    def create_dataset():
+    def create_dataset(eval_ratio):
         cmd = 'melody_rnn_create_dataset \
                 --config=attention_rnn \
                 --input=./tmp/notesequences.tfrecord \
                 --output_dir=./tmp/melody_rnn/sequence_examples \
-                --eval_ratio=0.10'
+                --eval_ratio='+eval_ratio
 
         system(cmd)
 
-    def train():
+    def train(steps):
 
         cmd = 'melody_rnn_train \
                 --config=attention_rnn \
                 --run_dir=./tmp/melody_rnn/logdir/run1 \
                 --sequence_example_file=./tmp/melody_rnn/sequence_examples/training_melodies.tfrecord \
                 --hparams="batch_size=64,rnn_layer_sizes=[64,64]" \
-                --num_training_steps=1000'
+                --num_training_steps='+steps
 
         system(cmd)
 
-    def generate(outdir):
+    def generate(outdir,num_outputs):
 
         if outdir is None:
             outdir = './tmp/melody_rnn/generated'
@@ -50,7 +50,7 @@ class Koji():
                 --config=attention_rnn \
                 --run_dir=./tmp/melody_rnn/logdir/run1 \
                 --output_dir=' + outdir + ' \
-                --num_outputs=10 \
+                --num_outputs='+ num_outputs + '\
                 --num_steps=128 \
                 --hparams="batch_size=64,rnn_layer_sizes=[64,64]" \
                 --primer_melody="[60]"'
@@ -60,33 +60,43 @@ class Koji():
     def run(argv):
         genre = None
         outdir = None
+        steps = '1000'
+        eval_ratio = '0.1'
+        num_outputs = '10'
+
         try:
-           opts, args = getopt.getopt(argv,"hg:o:",["genre=","outdir="])
+           opts, args = getopt.getopt(argv,"hg:o:s:e:n:",["genre=","outdir=","steps=","eval_ratio=","num_outputs="])
         except getopt.GetoptError:
-            print ('koji.py -g <genre> [-o <outdir>]')
+            print ('koji.py -g <genre> [-o <outdir> -s <steps> -e <eval ratio> -n <number of outputs>]')
             sys.exit(2)
         for opt, arg in opts:
             if opt == '-h':
-                    print ('koji.py -g <genre> [-o <outdir>]')
+                    print ('koji.py -g <genre> [-o <outdir> -s <steps> -e <eval ratio> -n <number of outputs>]')
                     print('Implemented genres:')
                     dirs = listdir("data/inputs/")
                     for genre in dirs:
                         print(' ', '-',genre) 
                     sys.exit()
-            elif opt in ("-g", "--ifile"):
+            elif opt in ("-g", "--genre"):
                 genre = arg
-            elif opt in ("-o", "--ofile"):
-                outdir = arg  
+            elif opt in ("-o", "--outdir"):
+                outdir = arg
+            elif opt in ("-s", '--steps'):
+                steps = arg
+            elif opt in ("-e", "--eval_ratio"):
+                eval_ratio = arg
+            elif opt in ("-n", "--num_outputs"):
+                num_outputs = arg 
         
         if(genre is None):
-            print('koji.py -g <genre> [-o <outdir>]')
+            print('koji.py -g <genre> [-o <outdir> -s <steps> -e <eval ratio> -n <number of outputs>]')
             sys.exit(2)
 
         path = Koji.genre_path(genre)
 
         Koji.convert_directory(path)
-        Koji.create_dataset()
-        Koji.train()
-        Koji.generate(outdir)
+        Koji.create_dataset(eval_ratio)
+        Koji.train(steps)
+        Koji.generate(outdir,num_outputs)
 
 Koji.run(sys.argv[1:])
