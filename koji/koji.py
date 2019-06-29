7e64c128d1ff50427afd1c59171e961108b426a1
Arthur Assis <arthur120496@gmail.com>
2019-06-29 12:37:10 -0400

    Creates basic cli structure

--- koji/koji.py
+++ koji/koji.py
@@ -1,27 +1,22 @@
 from os import system
+from os import listdir
+from os.path import exists
 import sys, getopt
 
 class Koji():
 
-    def convert_directory():
-        inputfile = ''
-        outputfile = ''
-        try:
-           opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-        except getopt.GetoptError:
-            print 'koji.py -i <inputfile> -o <outputfile>'
-            sys.exit(2)
-        for opt, arg in opts:
-           if opt == '-h':
-                    print 'test.py -i <inputfile>'
-                    sys.exit()
-           elif opt in ("-i", "--ifile"):
-                inputfile = arg
-            elif opt in ("-o", "--ofile"):
-                outputfile = arg  
-        print 'Input file is "', inputfile
+    def genre_path(genre):
+        path = "data/inputs/"+genre
+        if(exists(path)):
+            return path
+        else:
+            print("koji.py: the genre \'" + genre + "\' is not implemented yet.")
+            print("to add a new genre https://github.com/deeplearningunb/koji#adding-new-genres read this tutorial.")
+            sys.exit(2) 
+
+    def convert_directory(path):
         cmd = 'convert_dir_to_note_sequences \
-                --input_dir=./data/inputs \
+                --input_dir=' + path + ' \
                 --output_file=./tmp/notesequences.tfrecord \
                 --recursive'
 
@@ -47,12 +42,14 @@ class Koji():
 
         system(cmd)
 
-    def generate():
+    def generate(outdir):
 
+        if outdir is None:
+            outdir = './tmp/melody_rnn/generated'
         cmd = 'melody_rnn_generate \
                 --config=attention_rnn \
                 --run_dir=./tmp/melody_rnn/logdir/run1 \
-                --output_dir=./tmp/melody_rnn/generated \
+                --output_dir=' + outdir + ' \
                 --num_outputs=10 \
                 --num_steps=128 \
                 --hparams="batch_size=64,rnn_layer_sizes=[64,64]" \
@@ -60,10 +57,36 @@ class Koji():
             
         system(cmd)
 
-    def run():
-        Koji.convert_directory()
+    def run(argv):
+        genre = None
+        outdir = None
+        try:
+           opts, args = getopt.getopt(argv,"hg:o:",["genre=","outdir="])
+        except getopt.GetoptError:
+            print ('koji.py -g <genre> [-o <outdir>]')
+            sys.exit(2)
+        for opt, arg in opts:
+            if opt == '-h':
+                    print ('koji.py -g <genre> [-o <outdir>]')
+                    print('Implemented genres:')
+                    dirs = listdir("data/inputs/")
+                    for genre in dirs:
+                        print(' ', '-',genre) 
+                    sys.exit()
+            elif opt in ("-g", "--ifile"):
+                genre = arg
+            elif opt in ("-o", "--ofile"):
+                outdir = arg  
+        
+        if(genre is None):
+            print('koji.py -g <genre> [-o <outdir>]')
+            sys.exit(2)
+
+        path = Koji.genre_path(genre)
+
+        Koji.convert_directory(path)
         Koji.create_dataset()
         Koji.train()
-        Koji.generate()
+        Koji.generate(outdir)
 
 Koji.run(sys.argv[1:])
